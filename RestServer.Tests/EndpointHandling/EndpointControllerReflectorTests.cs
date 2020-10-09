3563c6cf849594767942600c7af8c6d97d59007c
Amsh285 <58710888+Amsh285@users.noreply.github.com>
2020-10-09 07:50:13 +0200

    updated .gitignore

---
+++ RestServer.Tests/EndpointHandling/EndpointControllerReflectorTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using RestServer.CommunicationObjects;
+using RestServer.EndpointHandling;
+using RestServer.EndpointHandling.Attributes;
+using RestServer.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestServer.UnitTests.EndpointHandling
+{
+    [TestFixture]
+    public sealed class EndpointControllerReflectorTests
+    {
+        [TestCase("", HttpVerb.GET)]
+        [TestCase("SubResources", HttpVerb.GET)]
+        [TestCase("1", HttpVerb.GET)]
+        [TestCase("1/SubResource", HttpVerb.GET)]
+        [TestCase("1/SubResource/1", HttpVerb.GET)]
+        public void ActionRoutingWorksAsIntended(string actionRoute, HttpVerb method)
+        {
+            string[] actionRouteSegments = actionRoute
+                    .Split("/")
+                    .Where(segment => !string.IsNullOrWhiteSpace(segment))
+                    .ToArray();
+
+            RouteActionMatch result = EndpointControllerReflector.SearchRouteActionMatch(actionRouteSegments, method, typeof(TestController));
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.HttpMethod);
+
+            Assert.AreEqual(typeof(TestController), result.Controller);
+            Assert.AreEqual(HttpVerbAttributeTypeMapping[method], result.HttpMethod.GetType());
+            RouteTemplateIsValidForActionRoute(actionRouteSegments, result.HttpMethod);
+        }
+
+        private static void RouteTemplateIsValidForActionRoute(string[] actionRouteSegments, HttpMethodAttribute actual)
+        {
+            Assert.IsNotNull(actual);
+
+            string[] templatePathSegments = actual.GetTemplatePathSegments();
+            Assert.AreEqual(actionRouteSegments.Length, templatePathSegments.Length);
+
+            for (int i = 0; i < templatePathSegments.Length; ++i)
+            {
+                if (!templatePathSegments[i].StartsWith('{') && !templatePathSegments[i].EndsWith('}'))
+                    StringAssert.AreEqualIgnoringCase(actionRouteSegments[i], templatePathSegments[i]);
+            }
+        }
+
+        private static readonly IReadOnlyDictionary<HttpVerb, Type> HttpVerbAttributeTypeMapping =
+            new Dictionary<HttpVerb, Type>() {
+                { HttpVerb.GET, typeof(HttpGetAttribute) }, { HttpVerb.POST, typeof(HttpPostAttribute) },
+                { HttpVerb.PUT, typeof(HttpPutAttribute) }, { HttpVerb.DELETE, typeof(HttpDeleteAttribute) }
+            };
+    }
+}
---
+++ RestServer.Tests/TestController.cs
@@ -0,0 +1,51 @@
+﻿using RestServer.CommunicationObjects;
+using RestServer.EndpointHandling;
+using RestServer.EndpointHandling.Attributes;
+
+namespace RestServer.Tests
+{
+    public sealed class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTests()
+        {
+            return Ok();
+        }
+
+        [HttpGet("SubResources")]
+        public IActionResult GetTestsSubResources()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{someID}")]
+        public IActionResult GetTestWithID(string someID)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{someID}/SubResource")]
+        public IActionResult GetTestWithIDAndSingleSubResource(string someID)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{someID}/SubResource/{someID}")]
+        public IActionResult GetTestWithIDAndIdentifiedSubResource(string someID, string someSubResourceID)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult PostTest()
+        {
+            return Ok();
+        }
+
+        [HttpPost("{someID}")]
+        public IActionResult PostTestWithId()
+        {
+            return Ok();
+        }
+    }
+}
